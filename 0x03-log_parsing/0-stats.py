364856f043f16e8d98e71b8d2b18166c50c9d5ab
Gypsy-Zobam <zo_bam@yahoo.com>
2023-07-20 17:08:05 +0000

    log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,38 @@
 #!/usr/bin/python3
-
-"""Script that reads stdin line by line and computes metrics"""
+"""reads stdin line by line and computes metrics"""
 
 import sys
 
-
-def printsts(dic, size):
-    """ WWPrints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
-
-
-sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
-
-count = 0
-size = 0
+total_size = 0
+counter = 0
+codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+dict_counter = {'200': 0, '301': 0,
+                '400': 0, '401': 0,
+                '403': 0, '404': 0,
+                '405': 0, '500': 0}
 
 try:
     for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printsts(sts, size)
+        list_args = line.split(" ")
+        if len(list_args) > 2:
+            code = list_args[-2]
+            size = list_args[-1]
+            if code in codes:
+                dict_counter[code] += 1
+            total_size += int(size)
+            counter += 1
 
-        stlist = line.split()
-        count += 1
+        if counter == 10:
+            print("File size: {:d}".format(total_size))
+            for k, v in sorted(dict_counter.items()):
+                if v != 0:
+                    print("{}: {:d}".format(k, v))
+                    counter = 0
 
-        try:
-            size += int(stlist[-1])
-        except:
-            pass
-
-        try:
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
-        except:
-            pass
-    printsts(sts, size)
-
-
-except KeyboardInterrupt:
-    printsts(sts, size)
-    raise
+except Exception:
+    pass
+finally:
+    print("File size: {}".format(total_size))
+    for k, v in sorted(dict_counter.items()):
+        if v != 0:
+            print("{}: {}".format(k, v))
